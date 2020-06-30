e6dddeb4aaa8e87fed5ed8fe3227c585c507161b
Volodymyr <snu-snu@ukr.net>
2020-06-30 11:30:12 +0300

    late commit, done a lot

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let header = document.querySelector("header");
+header.addEventListener("click", function() {
+	header.classList.toggle("header")
+	header.classList.toggle("scrolled")
+})
+
+
+
+
+// function setHeader () {
+// 	if(window.scrollTop() > 100) {
+// 		header.addClass('scrolled');
+// 	} else {
+// 		header.removeClass("scrolled")
+// 	}
+// }
